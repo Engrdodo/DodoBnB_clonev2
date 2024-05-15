ff5969d5f11e75ce8135b78c51dc2edbcc756492
Akinola David Olawale <akinoladavidolawale@gmail.com>
2024-05-15 22:32:00 +0100

    completing the base model

---
+++ models/base_model.py
@@ -0,0 +1,21 @@
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        self.updated_at = datetime()
+        
+    def to_dict(self):
+        dict_representation = self.__dict__.copy()
+        dict_representation['__class__'] = self.__class__.__name__
+        dict_representation['created_at'] = self.created_at.isoformat()
+        dict_representation['updated_at'] = self.updated_at.isoformat()
+        return dict_representation
